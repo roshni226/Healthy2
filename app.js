687f3025c97605908906a7639893cc25ddddb325
unknown <roshu.joshi@gmail.com>
2024-09-22 23:11:59 +0530

    Updated app.listen to bind to 0.0.0.0 for external access

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.post('/add-client', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
